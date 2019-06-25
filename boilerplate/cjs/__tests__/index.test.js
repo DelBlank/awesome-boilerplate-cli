5586cbdb99faf012d8d489f233425d44f8b0e492
chongya <chongya@wacai.com>
2019-06-25 22:44:17 +0800

    refactor(boilerplate): cjs/__tests__/index.test.js 文件提取 entry 到 公共域

--- boilerplate/cjs/__tests__/index.test.js
+++ boilerplate/cjs/__tests__/index.test.js
@@ -1,6 +1,7 @@
+const entry = require('index.js')
+
 describe('test src/index.js', () => {
   it('should get right exports', () => {
-    const entry = require('index.js')
     expect(entry()).toBe(`this file is source code entry`)
   })
 })
